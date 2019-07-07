269a18006ea76eafdc93cb9fc826879b93120ca2
kirandeep123 <me@kiran.cc>
2019-07-07 20:53:09 +0530

    Updates

---
+++ precache-manifest.b5dbc16c92492e9d099a2bc9aefbefbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0533d3ad9dd397117e9bbca082cd4a23",
+    "url": "/Wisdom-Pet-Medicine/index.html"
+  },
+  {
+    "revision": "c4734e3ddb8f5acd8042",
+    "url": "/Wisdom-Pet-Medicine/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "fa2b896138be0ee6b0da",
+    "url": "/Wisdom-Pet-Medicine/static/css/main.91e4948d.chunk.css"
+  },
+  {
+    "revision": "c4734e3ddb8f5acd8042",
+    "url": "/Wisdom-Pet-Medicine/static/js/2.2e33fbbb.chunk.js"
+  },
+  {
+    "revision": "fa2b896138be0ee6b0da",
+    "url": "/Wisdom-Pet-Medicine/static/js/main.dbdbaa15.chunk.js"
+  },
+  {
+    "revision": "2bcfd3595a51cb67174b",
+    "url": "/Wisdom-Pet-Medicine/static/js/runtime~main.ee3250eb.js"
+  }
+]);
