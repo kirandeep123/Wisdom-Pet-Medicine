2be9b1f30ab6cb7e759548f9af3b768222f9dc02
kirandeep123 <me@kiran.cc>
2019-07-07 16:25:06 +0530

    listing and adding appointments

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,22 @@ import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
-
+import {without} from 'lodash';
+import { template } from '@babel/core';
 
 class App extends React.Component {
 
   constructor(){
     super();
     this.state ={
-      myappointments:[]
+      myappointments:[],
+      formDisplay:false,
+      lastIndex:0 
     }
+    this.deleteAppointment =this.deleteAppointment.bind(this);
+    this.toggleForm =this.toggleForm.bind(this);
+    this.addAppointment =this.addAppointment.bind(this);
+
   }
 
   componentDidMount(){
@@ -19,6 +26,8 @@ class App extends React.Component {
     .then(response => response.json())
     .then(result => {
         const apts =result.map(item =>{
+          item.aptId =this.state.lastIndex;
+          this.setState( {lastIndex :this.state.lastIndex +1})
           return item;
         })
 
@@ -27,22 +36,43 @@ class App extends React.Component {
         })
     })
   }
+
+  deleteAppointment(apt){
+    let tempApts =this.state.myappointments;
+    tempApts =without(tempApts,apt)
+
+    this.setState({myappointments :tempApts})
+  }
+
+
+  addAppointment(apt){
+    let apts =this.state.myappointments;
+    apt.aptId =this.state.lastIndex;
+    apts.unshift(apt);
+    this.setState({myappointments :apts,
+      lastIndex: this.state.lastIndex+1
+    })
+  }
+  toggleForm(){
+    this.setState({formDisplay:!this.state.formDisplay})
+  }
   
   render(){
+
+  
   return (
     <main className="page bg-white" id="petratings">
     <div className="container">
-      <div cclassNamelass="row">
+      <div className="row">
         <div className="col-md-12 bg-white">
           <div className="container">
-          {this.state.myName}
-          <AddAppointments/>
+          <AddAppointments formDisplay ={this.state.formDisplay} toggleForm ={this.toggleForm} addAppointment ={this.addAppointment}/>
           <SearchAppointments/>
-          <ListAppointments/>
+          <ListAppointments appointments ={this.state.myappointments} deleteAppointment ={this.deleteAppointment}/>
           </div>
         </div>
       </div>
-    </div>
+    </div> 
   </main>
   );
 }
